31ca935e73688921dcd0f0a5dce4004df735e07b
unknown <you@example.com>
2019-12-17 00:48:59 +0100

    Player sprites & animations
    - Created player sprite-sheet
    - Created player animations (front/back idle, front/back walking)

---
+++ MobileRPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movespeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Joystick joystick;
+    public Animator animator;
+
+    void Update()
+    {
+        // movement.x = Input.GetAxisRaw("Horizontal");
+        // movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
+
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate(){
+            rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}
