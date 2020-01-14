52beb61cbdb9c5fcbfd4bb71040a2b6c134f1154
unknown <you@example.com>
2020-01-14 21:55:05 +0100

    Push1
    - Added player Animations & movement using Joystick controller
    - Added basic world assets (Tree1, Rock1, Ground1)

--- MobileRPG/Assets/Scripts/Player/PlayerMovement.cs
+++ MobileRPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float movespeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
-    public Joystick joystick;
+    public Joystick leftJoystick;
+    public Joystick rightJoystick;
     public Animator animator;
 
     void Update()
@@ -15,14 +16,25 @@ public class PlayerMovement : MonoBehaviour
         // movement.x = Input.GetAxisRaw("Horizontal");
         // movement.y = Input.GetAxisRaw("Vertical");
 
-        movement.x = joystick.Horizontal;
-        movement.y = joystick.Vertical;
-
-        animator.SetFloat("Vertical", movement.y);
+        movement.x = leftJoystick.Horizontal;
+        movement.y = leftJoystick.Vertical;
+        
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Vertical", Mathf.Sign(leftJoystick.Vertical));
+        SaveLastVertPos();
     }
 
     void FixedUpdate(){
             rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+
+    void SaveLastVertPos() {
+        if (leftJoystick.Vertical > .01) {
+            animator.SetFloat("LastVertPos", 1);
+        } else if (leftJoystick.Vertical < -.01) {
+            animator.SetFloat("LastVertPos", -1);
+        } else if (leftJoystick.Vertical == 0) {
+            return;
+        }
     }
 }
