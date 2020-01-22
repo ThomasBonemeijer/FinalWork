3900bed8e0a641f7338b7a811aecd61442c09336
unknown <you@example.com>
2020-01-22 17:35:01 +0100

    Inventory/Crafting-system (Functionality)
    - Added drag and drop functionality to picked up items in the inventory
    - Added the already created remove item functionality to the drag and drop feature

---
+++ MobileRPG/Assets/Scripts/UI/Inventory/IconItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class IconItem : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    
+    Item item;
+    // public Button removeButton;
+    public Transform inventoryPannel;
+    public GameObject inventorySlot;
+    
+    public void OnDrag(PointerEventData eventData) {
+        transform.position = Input.mousePosition;
+        // removeButton.image.enabled = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        RectTransform invPanel = inventoryPannel as RectTransform;
+
+        if (!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition)) {
+            Debug.Log("Remove Item");
+            transform.localPosition = Vector3.zero;
+            // removeButton.image.enabled = true;
+            inventorySlot.GetComponent<InventorySlot>().OnRemoveButton();
+        } else {
+            Debug.Log("Reset Item Position");
+            transform.localPosition = Vector3.zero;
+            // removeButton.image.enabled = false;
+        }
+    }
+}
