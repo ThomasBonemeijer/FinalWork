4bc86cf08f8111b00ca68af51fc2306b0250c89b
unknown <you@example.com>
2020-02-29 20:10:42 +0100

    UI (Notifications-panel)
    - Created notification panel canvas with the ability to open it and assign the needed information dynamically.

--- MobileRPG/Assets/Scripts/UI/CanvasHandler.cs
+++ MobileRPG/Assets/Scripts/UI/CanvasHandler.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CanvasHandler : MonoBehaviour
 {
     public Canvas MainUI;
     public Canvas BagUI;
+    public Canvas notificationScreen;
     public GameObject cs1;
     public GameObject cs2;
 
@@ -14,16 +16,24 @@ public class CanvasHandler : MonoBehaviour
     public GameObject gunLSlot;
 
     public void OpenBag () {
-        Time.timeScale = 0;
+        PauseGame(true);
         MainUI.enabled = false;
         BagUI.enabled = true;
     }
 
     public void CloseBag () {
-        Time.timeScale = 1;
+        PauseGame(false);
         BagUI.enabled = false;
         MainUI.enabled = true;
         ClearInvSlots();
+    }
+
+    public void PauseGame(bool pause) {
+        if (pause == true) {
+            Time.timeScale = 0;
+        } else if (pause == false) {
+            Time.timeScale = 1;
+        }
     }
 
     // Clears the crafting slots when closing the bag
@@ -33,5 +43,26 @@ public class CanvasHandler : MonoBehaviour
         BagUI.transform.Find("CraftingInventory").GetComponent<CraftingHandler>().ResetCraftingOutput();
         knifeLSlot.transform.GetChild(0).GetComponent<IconItem>().ClearLoadoutSlot();
         gunLSlot.transform.GetChild(0).GetComponent<IconItem>().ClearLoadoutSlot();
+    }
+
+    // Open the notification screen and set the content
+    public void OpenNotificationScreen(string title, string text, Sprite image) {
+        // TMP_Text closeBtn = notificationScreen.transform.Find("CloseButton").GetComponent<TMP_Text>();
+        TMP_Text nTitle = notificationScreen.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+        TMP_Text nText = notificationScreen.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<TMP_Text>();
+        Image nImage = notificationScreen.transform.GetChild(0).GetChild(0).GetChild(2).GetComponent<Image>();
+
+        CloseBag();
+        PauseGame(true);
+
+        notificationScreen.enabled = true;
+        nTitle.text = title;
+        nText.text = text;
+        nImage.sprite = image;
+    }
+
+    public void CloseNotificationScreen() {
+        notificationScreen.enabled = false;
+        PauseGame(false);
     }
 }
---
+++ MobileRPG/Assets/Scripts/World/NotificationPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationPoint : MonoBehaviour
+{
+    public GameObject gameManager;
+    public string title;
+    public string text;
+    public Sprite image;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.name == "Player") {
+            gameManager.GetComponent<CanvasHandler>().OpenNotificationScreen(title, text, image);
+            Destroy(gameObject);
+        }
+    }
+}
